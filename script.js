6157dee85cf125bee0cbed1679bb0e6f63796bc6
krutoishitposter <moneyloveunderwear@gmail.com>
2024-11-13 21:27:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Можно добавить дополнительную функциональность, если нужно
+document.querySelector('.download-btn').addEventListener('click', function() {
+    console.log('Download initiated!');
+});
